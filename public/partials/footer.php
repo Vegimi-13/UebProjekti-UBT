edac4a247442fad33d4da674b92dd21d52c0c80b
Vegim Ahmetaj <vegimahmetaj@gmail.com>
2025-01-30 19:53:02 +0100

    Krijimi i file-it te ndare footer.php per te bere include ne files tjera

---
+++ public/partials/footer.php
@@ -0,0 +1,51 @@
+<div>
+     <footer id="footer">
+        <div class="footer-container-row-1">
+
+            <div class="logo">
+                <svg width="74" height="74" viewBox="0 0 74 74" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M36.9997 20.0416L50.8747 12.3333L66.2913 21.4181V54.1234L52.4163 63.2083L21.583 44.7083V27.7499L52.4163 45.0386V29.2916L36.9997 20.0416ZM36.9997 20.0416L23.1247 12.3333L7.70907 21.5833L7.70801 53.9583L23.1247 63.2083L36.9997 53.9583"
+                        stroke="black" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
+                </svg>
+                <a href="#">Devjobs</a>
+
+            </div>
+        </div>
+        <div class="footer-pages">
+            <ul>
+                <li class="active-link"><a href="#">Find Jobs</a></li>
+                <li> <a href="./about.html">About Us</a></li>
+                <li> <a href="./pricing.html">Pricing</a></li>
+            </ul>
+            <div class="login-button">
+                <form action="./partials/SignIn.html">
+                    <input type="submit" value="Sign in" id="SignIn">
+                
+                </form>
+                <form action="./partials/SignUp.html">
+                    <input type="submit" value="Sign up" id="signup-button">
+                </form>
+                
+            </div>
+
+        </div>
+        <hr>
+
+
+
+
+
+        <div class="footer-copyright">
+            <ul>
+                <li>Terms</li>
+                <li>Privacy</li>
+                <li>Cookies</li>
+            </ul>
+            <div style="align-content: center;">
+                <p id="copyright">&copy; 2024 DevJobs. All rights reserved.</p>
+            </div>
+        </div>
+
+        </footer>
+    </div>

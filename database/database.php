f77c87538f8e09d2349cf5eba20fcf724f24889c
Vegim Ahmetaj <vegimahmetaj@gmail.com>
2025-01-30 19:21:21 +0100

    Lidhja me databazengit add database/database.php

--- database/database.php
+++ database/database.php
@@ -2,4 +2,31 @@
 
 
 
+   
+
+    class Database{
+        private $host = "localhost";
+        private $username = "root";
+        private $password = "";
+        private $dbname = "projekti_db";
+       
+
+
+        function getConnection(){
+            try {
+               $conn = new PDO("mysql:host={$this->host}; dbname={$this->dbname}", $this-> username, $this->password);
+               $conn -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+               $conn -> setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            
+               return $conn;
+            }catch(PDOException $e){
+                echo "DATABASE CONNECTION FAILED!!!" . $e->getMessage();
+            }
+        }
+
+    }
+
+
+
+
 ?>
